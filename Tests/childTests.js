60e833cee286431d638382ca696902e1aa10d903
Zachary Williams <wil11063@byui.edu>
2017-12-01 16:51:15 -0700

    Complete Testing Setup

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -2,30 +2,45 @@
 const tap = require('tap');
 
 function g1Tests(course, callback) {
-    // Tap tests for Gauntlet 3 go here
-    tap.pass('Success! Wheee!');
-    tap.fail('YOLO');
+    // Tap tests for Gauntlet 1 go here
+    tap.pass('Success! Wheee! 1');
+    // tap.fail('YOLO');
     callback(null, course);
 }
 
 function g2Tests(course, callback) {
-    // Tap tests for Gauntlet 3 go here
+    // Tap tests for Gauntlet 2 go here
+    tap.pass('Success! Wheee! 2');
     callback(null, course);
 }
 
 function g3Tests(course, callback) {
     // Tap tests for Gauntlet 3 go here
+    tap.pass('Success! Wheee! 3');
     callback(null, course);
 }
 
 function g4Tests(course, callback) {
-    // Tap tests for Gauntlet 3 go here
+    // Tap tests for Gauntlet 4 go here
+    tap.pass('Success! Wheee! 4');
     callback(null, course);
 }
 
-module.exports = {
-    gauntlet1: g1Tests,
-    gauntlet2: g2Tests,
-    gauntlet3: g3Tests,
-    gauntlet4: g4Tests,
-};
+module.exports = [
+        {
+            gauntlet: 1,
+            tests: g1Tests
+        },
+        {
+            gauntlet: 2,
+            tests: g2Tests
+        },
+        {
+            gauntlet: 3,
+            tests: g3Tests
+        },
+        {
+            gauntlet: 4,
+            tests: g4Tests
+        },
+];
