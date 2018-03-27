689670889af1b74241239ad6aba740994c8298f1
Zachary Williams <wil11063@byui.edu>
2018-03-27 10:39:05 -0600

    Add canvas wrapper to tests

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -1,21 +1,14 @@
 /* Dependencies */
 const tap = require('tap');
+const canvas = require('canvas-wrapper');
 
 module.exports = (course, callback) => {
     tap.test('child-template', (test) => {
 
         test.pass('potato');
-        test.pass('potato');
-        test.pass('potato');
-        test.pass('potato');
-        test.pass('potato');
-        test.pass('potato');
-        test.pass('potato');
-        test.pass('potato');
-        test.pass('potato');
-        test.pass('potato');
-        test.pass('potato');
-        test.pass('potato');
+        test.pass('tomato');
+        test.fail('avacado');
+
         test.end();
     });
 
