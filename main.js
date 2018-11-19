df6e5c4418d3c1937869a8031cc41e0a34b5611c
Danverde <gre12014@byui.edu>
2018-11-19 13:50:10 -0700

    updated comments

--- main.js
+++ main.js
@@ -14,12 +14,12 @@ module.exports = (course, stepCallback) => {
         // Remeber to include AT LEAST ONE course.log in your child module
         course.log('Table description', {column: 'value'});
     
-    
         /* You should never call the stepCallback with an error. We want the
         whole program to run when testing so we can catch all existing errors */
         stepCallback(null, course);
-        
+
     } catch(err) {
+        // catch all uncaught errors. Don't pass errors here on purpose
         course.error(err);
         stepCallback(null, course);
         return;
